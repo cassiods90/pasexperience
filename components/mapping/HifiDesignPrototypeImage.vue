b02a328d5af0dab977b7570233a14ea4eb01ee4a
pedroairserrano <70380325+pedroairserrano@users.noreply.github.com>
2023-12-28 10:24:17 +0000

    Update HifiDesignPrototypeImage.vue

--- components/mapping/HifiDesignPrototypeImage.vue
+++ components/mapping/HifiDesignPrototypeImage.vue
@@ -7,7 +7,7 @@
             <span class="text moreTitle medium text-color01">Would you like to interact with Mapping’s prototype?</span>
             <a
                 class="text semibold white d-flex justify-content-center align-items-center"
-                href="https://www.figma.com/proto/xt1nqPyAu3QENbyTmC97hQ/2.-pasexperience.com-(Reedi%C3%A7%C3%A3o-29-Set.)?type=design&node-id=1023-2089&t=nqaqjyw5iYhB7A80-0&scaling=min-zoom&page-id=760%3A191&starting-point-node-id=1023%3A2089&show-proto-sidebar=1"
+                href="https://www.figma.com/proto/xt1nqPyAu3QENbyTmC97hQ/2.-pasexperience.com-(Reedi%C3%A7%C3%A3o-29-Set.)?page-id=760%3A191&type=design&node-id=1349-2940&viewport=209%2C516%2C0.17&t=X9EAnngQ1qNkzgdU-8&scaling=min-zoom&starting-point-node-id=1349%3A2940&show-proto-sidebar=1&hide-ui=1"
                 target="_blank"
                 >Click Here</a
             >
